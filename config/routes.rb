3dd462bfa066bc5aaf86ab644dbe9eaa27ad772a
Juan Pablo Amaya <jp.amaya@codescrum.com>
2015-03-19 16:00:00 -0500

    Change user routes to use devise cas session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails3MongoidDevise::Application.routes.draw do
-  authenticated :user do
-    root :to => 'home#index'
+
+  devise_for :users, :controllers => { sessions: 'devise/cas_sessions' }
+  devise_for :users do
+    #redirect to dashboard after edit details
+    # get "/", :to => "devise/sessions#new"
   end
+
+  # authenticated :user do
+  #   root :to => 'home#index'
+  # end
   root :to => "home#index"
-  devise_for :users
+  # devise_for :users
   resources :users
 end
